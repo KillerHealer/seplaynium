a3fcbe417db562498dcfee4a10bdc00c8d8feaa7
KillerHealer <noam142196@gmail.com>
2022-08-12 01:53:02 +0300

    another test i thought for login

--- test_login_form.py
+++ test_login_form.py
@@ -9,9 +9,9 @@ chrome_options.add_experimental_option("detach", True)
 chrom_driver_path = "D:\pythonProject\chromedriver.exe"
 
 
-def test_login_form_logged_in():
+def get_to_login_page():
     """
-    tries to login with normal credentials and check if the page has swapped
+    gets page to login page
     :return:
     """
     driver = webdriver.Chrome(chrom_driver_path, chrome_options=chrome_options)
@@ -19,6 +19,15 @@ def test_login_form_logged_in():
     driver.get('http://automationpractice.com/index.php')
     login_btn = driver.find_element(By.CLASS_NAME, "login")
     login_btn.click()
+    return driver
+
+
+def test_login_form_logged_in():
+    """
+    tries to login with normal credentials and check if the page has swapped
+    :return:
+    """
+    driver = get_to_login_page()
     login_form = driver.find_element(By.XPATH, '//*[@id="login_form"]')
     email_input = login_form.find_element(By.XPATH, '//*[@id="email"]')
     email_input.send_keys("noam@gmail.com")
@@ -37,11 +46,7 @@ def test_login_form_logged_in_failed():
     tries to input wrong credentials and to find the specific error warning "Authentication failed."
     :return:
     """
-    driver = webdriver.Chrome(chrom_driver_path, chrome_options=chrome_options)
-    driver.maximize_window()
-    driver.get('http://automationpractice.com/index.php')
-    login_btn = driver.find_element(By.CLASS_NAME, "login")
-    login_btn.click()
+    driver = get_to_login_page()
     login_form = driver.find_element(By.XPATH, '//*[@id="login_form"]')
     email_input = login_form.find_element(By.XPATH, '//*[@id="email"]')
     email_input.send_keys("noam@gmail.com")
@@ -60,18 +65,14 @@ def test_login_form_without_password_failed():
     tries to input wrong credentials and to find the specific error warning "Password is required."
     :return:
     """
-    driver = webdriver.Chrome(chrom_driver_path, chrome_options=chrome_options)
-    driver.maximize_window()
-    driver.get('http://automationpractice.com/index.php')
-    login_btn = driver.find_element(By.CLASS_NAME, "login")
-    login_btn.click()
+    driver = get_to_login_page()
     login_form = driver.find_element(By.XPATH, '//*[@id="login_form"]')
     email_input = login_form.find_element(By.XPATH, '//*[@id="email"]')
     email_input.send_keys("noam@gmail.com")
     sign_in_btn = login_form.find_element(By.XPATH, '//*[@id="SubmitLogin"]')
     sign_in_btn.click()
     assert driver.find_element(By.XPATH, '//*[@id="center_column"]/div[1]') \
-               .find_element(By.XPATH, '//*[@id="center_column"]/div[1]/ol/li').text == "Password is required."
+        .find_element(By.XPATH, '//*[@id="center_column"]/div[1]/ol/li').text == "Password is required."
     time.sleep(3)
     driver.quit()
 
@@ -81,17 +82,21 @@ def test_login_form_without_email_failed():
     tries to input wrong credentials and to find the specific error warning "An email address required."
     :return:
     """
-    driver = webdriver.Chrome(chrom_driver_path, chrome_options=chrome_options)
-    driver.maximize_window()
-    driver.get('http://automationpractice.com/index.php')
-    login_btn = driver.find_element(By.CLASS_NAME, "login")
-    login_btn.click()
+    driver = get_to_login_page()
     login_form = driver.find_element(By.XPATH, '//*[@id="login_form"]')
     pass_input = login_form.find_element(By.XPATH, '//*[@id="passwd"]')
     pass_input.send_keys("Abcd1234@")
     sign_in_btn = login_form.find_element(By.XPATH, '//*[@id="SubmitLogin"]')
     sign_in_btn.click()
     assert driver.find_element(By.XPATH, '//*[@id="center_column"]/div[1]') \
-               .find_element(By.XPATH, '//*[@id="center_column"]/div[1]/ol/li').text == "An email address required."
+        .find_element(By.XPATH, '//*[@id="center_column"]/div[1]/ol/li').text == "An email address required."
+    time.sleep(3)
+    driver.quit()
+
+
+def test_forgot_password_btn():
+    driver = get_to_login_page()
+    driver.find_element(By.XPATH, '//a[text()="Forgot your password?"]').click()
+    assert driver.current_url == "http://automationpractice.com/index.php?controller=password"
     time.sleep(3)
     driver.quit()
