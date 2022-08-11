d2f4c78208a2e4ca5181f01f34b81eb63f9c6ae8
KillerHealer <noam142196@gmail.com>
2022-08-12 01:42:54 +0300

    test for buying a dress done

--- test_buy_dress.py
+++ test_buy_dress.py
@@ -1,4 +1,6 @@
 import re
+import time
+
 from playwright.sync_api import Page, expect
 
 
@@ -20,3 +22,31 @@ def test_buy_cheapest_dress(page: Page):
     sign_in_btn = page.locator("id=SubmitLogin")
     sign_in_btn.click()
     expect(page).to_have_url(re.compile(".*my-account"))
+    search_box = page.locator("id=search_query_top")
+    search_btn = page.locator("button.button-search")
+    search_box.type("summer")
+    search_btn.click()
+    time.sleep(4)
+    expect(page).to_have_url("http://automationpractice.com/"
+                             "index.php?controller=search&orderby=position&orderway=desc&search_query=summer"
+                             "&submit_search=")
+    products_list = page.locator('ul.product_list li')
+    prices = page.locator("ul.product_list li .product-price")
+    prices_list = []
+    for price in prices.all_inner_texts():
+        prices_list.append(re.sub('[^\d\.]', "", price))
+    cheapest_product = products_list.locator(f".product-container:has-text('${(min(prices_list))}')")
+    cheapest_product.hover()
+    page.wait_for_timeout(3000)
+    cheapest_product.locator("text='Add to cart'").click()
+    time.sleep(5)
+    page.locator("text='Proceed to checkout'").click()
+    time.sleep(2)
+    page.locator("#center_column >> text='Proceed to checkout'").click()
+    page.locator("button >> text='Proceed to checkout'").click()
+    checkbox = page.locator("input#cgv")
+    checkbox.click()
+    page.locator("button >> text='Proceed to checkout'").click()
+    page.locator("text='Pay by bank wire'").click()
+    page.locator("button >> text='I confirm my order'").click()
+    assert "Your order on My Store is complete" in page.locator('body').inner_html()
--- test_login_form.py
+++ test_login_form.py
@@ -2,9 +2,10 @@ import re
 from playwright.sync_api import Page, expect
 
 
-def test_login_form_logged_in(page: Page):
+def get_to_sign_in_page(page: Page):
     """
-    tries to login with normal credentials and check if the page has swapped
+    goes to the sign in page
+    :param page: the page has all the elements
     :return:
     """
     page.goto("http://automationpractice.com/index.php")
@@ -13,6 +14,15 @@ def test_login_form_logged_in(page: Page):
     expect(get_started).to_have_attribute("href", "http://automationpractice.com/index.php?controller=my-account")
     get_started.click()
     expect(page).to_have_url(re.compile(".*my-account"))
+
+
+def test_login_form_logged_in(page: Page):
+    """
+    tries to login with normal credentials and check if the page has swapped
+    :param page: the page has all the elements
+    :return:
+    """
+    get_to_sign_in_page(page)
     email_input = page.locator("id=email")
     email_input.type("noam@gmail.com")
     pass_input = page.locator("id=passwd")
@@ -26,14 +36,10 @@ def test_login_form_logged_in(page: Page):
 def test_login_form_logged_in_failed(page:Page):
     """
     tries to input wrong credentials and to find the specific error warning "Authentication failed."
+    :param page: the page has all the elements
     :return:
     """
-    page.goto("http://automationpractice.com/index.php")
-    expect(page).to_have_title(re.compile("My Store"))
-    get_started = page.locator('xpath=//*[@id="header"]/div[2]/div/div/nav/div[1]/a')
-    expect(get_started).to_have_attribute("href", "http://automationpractice.com/index.php?controller=my-account")
-    get_started.click()
-    expect(page).to_have_url(re.compile(".*my-account"))
+    get_to_sign_in_page(page)
     email_input = page.locator("id=email")
     email_input.type("noam@gmail.com")
     pass_input = page.locator("id=passwd")
@@ -47,14 +53,10 @@ def test_login_form_logged_in_failed(page:Page):
 def test_login_form_without_password_failed(page: Page):
     """
     tries to input wrong credentials and to find the specific error warning "Password is required."
+    :param page: the page has all the elements
     :return:
     """
-    page.goto("http://automationpractice.com/index.php")
-    expect(page).to_have_title(re.compile("My Store"))
-    get_started = page.locator('xpath=//*[@id="header"]/div[2]/div/div/nav/div[1]/a')
-    expect(get_started).to_have_attribute("href", "http://automationpractice.com/index.php?controller=my-account")
-    get_started.click()
-    expect(page).to_have_url(re.compile(".*my-account"))
+    get_to_sign_in_page(page)
     email_input = page.locator("id=email")
     email_input.type("noam@gmail.com")
     sign_in_btn = page.locator("id=SubmitLogin")
@@ -66,17 +68,26 @@ def test_login_form_without_password_failed(page: Page):
 def test_login_form_without_email_failed(page: Page):
     """
     tries to input wrong credentials and to find the specific error warning "An email address required."
+    :param page: the page has all the elements
     :return:
     """
-    page.goto("http://automationpractice.com/index.php")
-    expect(page).to_have_title(re.compile("My Store"))
-    get_started = page.locator('xpath=//*[@id="header"]/div[2]/div/div/nav/div[1]/a')
-    expect(get_started).to_have_attribute("href", "http://automationpractice.com/index.php?controller=my-account")
-    get_started.click()
-    expect(page).to_have_url(re.compile(".*my-account"))
+    get_to_sign_in_page(page)
     pass_input = page.locator("id=passwd")
     pass_input.type("Abcd134@")
     sign_in_btn = page.locator("id=SubmitLogin")
     sign_in_btn.click()
     expect(page).to_have_url(re.compile(".*authentication"))
     assert page.locator("text=An email address required.").text_content() == "An email address required."
+
+
+def test_click_forgot_password(page: Page):
+    """
+    tries to click to the 'Forgot your password?' button
+    :param page: the page has all the elements
+    :return:
+    """
+    get_to_sign_in_page(page)
+    page.locator("text=Forgot your password?").click()
+    assert page.url == "http://automationpractice.com/index.php?controller=password"
+
+
